62a09e9e6672d020c4416f4da7c75af26fcc9980
GeisianeMaia <geisiane.maia@gmail.com>
2024-01-27 08:04:26 -0300

    estrutura de pasta backend

---
+++ esmaltaria/esmaltaria/src/test/java/br/com/fiap/esmaltaria/esmaltaria/EsmaltariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.esmaltaria.esmaltaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsmaltariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
